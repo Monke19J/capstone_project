a62c59f8936335b68ee5f11cbf89b828042ae284
Monke19J <eronjamesador@gmail.com>
2025-09-22 00:34:36 +0800

    Updated DB Sept. 22, 2025, 12:34 am

--- j-jk-mets.sql
+++ j-jk-mets.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 20, 2025 at 08:51 PM
+-- Generation Time: Sep 21, 2025 at 06:33 PM
 -- Server version: 11.5.2-MariaDB
 -- PHP Version: 8.2.12
 
@@ -80,33 +80,32 @@ CREATE TABLE `reagents` (
   `reagent_id` int(11) NOT NULL,
   `reagent_type` varchar(50) NOT NULL COMMENT 'chemistry,\r\nimmunology,\r\nhematology',
   `reagent_name` varchar(150) NOT NULL,
-  `quantity` int(11) NOT NULL,
-  `unit` varchar(50) NOT NULL,
   `min_quantity` int(11) DEFAULT NULL,
   `category` varchar(50) NOT NULL COMMENT 'reagents,\r\ncalibrators,\r\ncontrols,\r\nelectrolyte,\r\nconsumables',
   `reagent_img` varchar(255) NOT NULL,
   `item_description` varchar(255) DEFAULT NULL,
   `test_kit` varchar(100) DEFAULT NULL,
-  `packaging` text DEFAULT NULL
+  `packaging` text DEFAULT NULL,
+  `reagent_status` varchar(50) NOT NULL DEFAULT 'active' COMMENT 'active, inactive'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `reagents`
 --
 
-INSERT INTO `reagents` (`reagent_id`, `reagent_type`, `reagent_name`, `quantity`, `unit`, `min_quantity`, `category`, `reagent_img`, `item_description`, `test_kit`, `packaging`) VALUES
-(1, 'chemistry', 'hello world', 10, 'ft', 1, 'consumables', './images/no_available_img.jpg', '', '', NULL),
-(2, 'chemistry', 'abcdefg', 5, 'Units', 1, 'reagents', './images/no_available_img.jpg', '', '', NULL),
-(3, 'chemistry', 'testing 2', 123, 'cm', 1, 'calibrators', './images/no_available_img.jpg', '', '', NULL),
-(4, 'chemistry', 'image-testing', 123, 'Units', 1, 'controls', './images/no_available_img.jpg', '', '', NULL),
-(5, 'chemistry', 'testin', 1234, 'Units', 1, 'consumables', './images/no_available_img.jpg', '', '', NULL),
-(6, 'chemistry', 'image-testin', 123, 'Units', 123, 'reagents', '', '', '', NULL),
-(7, 'chemistry', 'image', 123, 'kg', 1, 'electrolyte', './product_img/img_68c035ffa74640.81130042.png', '', '', NULL),
-(8, 'chemistry', '6999', 1, 'in', 20, 'consumables', './product_img/img_68c13d4f9df6d8.49735014.png', '', '', NULL),
-(9, 'chemistry', 'testingAlertReagent', 10, 'yd', 3, 'consumables', './product_img/img_68c4212f37d537.75553195.jpg', '', '', NULL),
-(10, 'chemistry', 'omegalol', 1, 'Units', 1, 'reagents', './product_img/img_68c439257e40c1.71388137.jpg', '1', '1', '1'),
-(11, 'chemistry', 'testing', 0, '', 5, 'reagents', './product_img/img_68cd068c919215.58265284.png', 'testing', 'testing', 'testing'),
-(12, 'chemistry', 'edit-good', 0, '', 4567, 'controls', 'uploads/1758279527_m0bv61rf4im51.png', 'success', 'success', 'success');
+INSERT INTO `reagents` (`reagent_id`, `reagent_type`, `reagent_name`, `min_quantity`, `category`, `reagent_img`, `item_description`, `test_kit`, `packaging`, `reagent_status`) VALUES
+(1, 'chemistry', 'Reagent 1', 1, 'calibrators', 'uploads/1758471995_1.png', '', '', '', 'active'),
+(2, 'chemistry', 'Reagent 2', 1, 'reagents', 'uploads/1758472004_2.png', '', '', '', 'active'),
+(3, 'chemistry', 'Reagent 3', 1, 'calibrators', 'uploads/1758472012_3.png', '', '', '', 'active'),
+(4, 'chemistry', 'Reagent 4', 1, 'controls', 'uploads/1758472019_4.png', '', '', '', 'active'),
+(5, 'chemistry', 'Reagent 5', 1, 'consumables', 'uploads/1758472033_5.png', '', '', '', 'active'),
+(6, 'chemistry', 'Reagent 6', 123, 'reagents', 'uploads/1758472051_6.png', '', '', '', 'active'),
+(7, 'chemistry', 'Reagent 7', 1, 'electrolyte', 'uploads/1758472073_7.png', '', '', '', 'active'),
+(8, 'chemistry', 'Reagent 8', 20, 'consumables', 'uploads/1758472085_8.png', '', '', '', 'active'),
+(9, 'chemistry', 'Reagent 10', 3, 'consumables', 'uploads/1758472101_10.png', '', '', '', 'active'),
+(10, 'chemistry', 'omegalol', 1, 'reagents', './product_img/img_68c439257e40c1.71388137.jpg', '1', '1', '1', 'inactive'),
+(11, 'chemistry', 'testing', 5, 'reagents', './product_img/img_68cd068c919215.58265284.png', 'testing', 'testing', 'testing', 'active'),
+(12, 'chemistry', 'edit-good', 4567, 'controls', 'uploads/1758279527_m0bv61rf4im51.png', 'success', 'success', 'success', 'active');
 
 -- --------------------------------------------------------
 
@@ -123,15 +122,21 @@ CREATE TABLE `reagent_stock` (
   `expiry_date` date NOT NULL,
   `quantity` int(11) NOT NULL DEFAULT 0,
   `created_at` timestamp NULL DEFAULT current_timestamp(),
-  `updated_at` timestamp NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+  `updated_at` timestamp NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `stock_status` enum('active','inactive') DEFAULT 'active'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `reagent_stock`
 --
 
-INSERT INTO `reagent_stock` (`stock_id`, `reagent_id`, `lot_no`, `distributor`, `date_arrived`, `expiry_date`, `quantity`, `created_at`, `updated_at`) VALUES
-(1, 1, '1', 'Distributor A', '2025-09-20', '2025-11-20', 43, '2025-09-20 15:12:36', '2025-09-20 18:43:49');
+INSERT INTO `reagent_stock` (`stock_id`, `reagent_id`, `lot_no`, `distributor`, `date_arrived`, `expiry_date`, `quantity`, `created_at`, `updated_at`, `stock_status`) VALUES
+(1, 1, '1', 'Distributor A', '2025-09-20', '2025-11-20', 51, '2025-09-20 15:12:36', '2025-09-21 16:13:54', 'active'),
+(2, 1, '2', 'Distributor B', '2025-09-21', '2025-12-29', 16, '2025-09-21 05:09:44', '2025-09-21 16:13:54', 'active'),
+(3, 11, '1234', 'Distributor A', '2025-09-21', '2025-12-31', 50, '2025-09-21 09:32:38', '2025-09-21 09:32:38', 'active'),
+(4, 1, '3', 'Distributor A', '2025-09-21', '2025-11-21', 40, '2025-09-21 12:32:23', '2025-09-21 16:13:54', 'active'),
+(5, 2, '69420', 'Distributor A', '2025-09-22', '2025-09-22', 100, '2025-09-21 16:01:47', '2025-09-21 16:13:54', 'active'),
+(6, 12, '1234', 'Distributor A', '2025-09-30', '2025-09-30', 10, '2025-09-21 16:28:59', '2025-09-21 16:29:07', 'inactive');
 
 -- --------------------------------------------------------
 
@@ -165,7 +170,16 @@ INSERT INTO `stock_history` (`history_id`, `reagent_id`, `stock_id`, `action_typ
 (7, 1, NULL, 'add', 10, NULL, '2025-09-04', '2025-09-20 18:06:25', NULL),
 (8, 1, NULL, 'add', 21, NULL, '2025-09-01', '2025-09-20 18:15:12', NULL),
 (9, 1, NULL, 'remove', 30, NULL, '2025-09-21', '2025-09-20 18:15:33', NULL),
-(14, 1, NULL, 'add', 21, NULL, '2025-09-21', '2025-09-20 18:43:49', NULL);
+(14, 1, NULL, 'add', 21, NULL, '2025-09-21', '2025-09-20 18:43:49', NULL),
+(15, 1, NULL, 'add', 4, 0, '2025-09-21', '2025-09-21 05:10:15', NULL),
+(16, 1, NULL, 'add', 4, NULL, '2025-09-21', '2025-09-21 05:10:34', NULL),
+(17, 1, NULL, 'add', 4, 0, '2025-09-21', '2025-09-21 05:15:33', NULL),
+(18, 1, NULL, 'add', 4, NULL, '2025-09-21', '2025-09-21 05:15:51', NULL),
+(19, 1, NULL, 'add', 4, NULL, '2025-09-02', '2025-09-21 12:02:46', NULL),
+(20, 1, 4, 'add', 20, 0, '2025-09-21', '2025-09-21 12:34:45', NULL),
+(21, 1, 4, 'remove', 20, 0, '2025-09-21', '2025-09-21 12:36:30', NULL),
+(22, 1, 4, 'add', 15, NULL, '2025-09-30', '2025-09-21 15:29:16', NULL),
+(23, 1, 4, 'add', 15, NULL, '2025-09-30', '2025-09-21 15:29:43', NULL);
 
 -- --------------------------------------------------------
 
@@ -260,13 +274,13 @@ ALTER TABLE `reagents`
 -- AUTO_INCREMENT for table `reagent_stock`
 --
 ALTER TABLE `reagent_stock`
-  MODIFY `stock_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `stock_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `stock_history`
 --
 ALTER TABLE `stock_history`
-  MODIFY `history_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `history_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT for table `users`
